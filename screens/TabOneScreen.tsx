9bf4a101ff091833280b161ca3b21b7c53523665
prasoonjha <prasoonjha69@gmail.com>
2022-03-27 21:18:15 +0530

    simple pan responder

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,18 +1,7 @@
+import { useRef } from 'react';
 import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Animated, Text, View, PanResponder } from 'react-native';
 import { RootTabScreenProps } from '../types';
-
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -20,13 +9,64 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  ball: {
+    backgroundColor: '#DC143C',
+    height: 100,
+    width: 100,
+    borderRadius: 10,
   },
 });
+
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const position = useRef(new Animated.ValueXY()).current;
+  const size = useRef(new Animated.Value(0.5)).current;
+  const rotateRef = useRef(new Animated.Value(0)).current;
+  const rotateInterpolate = rotateRef.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+  const animateSize = {
+    transform: [
+      {
+        scale: size,
+      },
+      {
+        rotate: rotateInterpolate,
+      },
+    ],
+  };
+
+  const panResponder = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderRelease: () => {
+      Animated.parallel([
+        Animated.timing(rotateRef, {
+          toValue: 1,
+          useNativeDriver: false,
+        }),
+        Animated.timing(size, {
+          toValue: 2,
+          useNativeDriver: false,
+        }),
+        Animated.spring(
+          position,
+          {
+            toValue: { x: 0, y: 0 },
+            useNativeDriver: false,
+          }, // Back to zero
+        ),
+      ]).start();
+    },
+    onPanResponderMove: Animated.event([null, { dx: position.x, dy: position.y }], {
+      useNativeDriver: false,
+    }),
+  });
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={[styles.ball, position.getLayout(), animateSize]}
+        {...panResponder.panHandlers}
+      />
+    </View>
+  );
+}
